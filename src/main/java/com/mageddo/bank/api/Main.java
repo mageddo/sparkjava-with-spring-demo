2534af22ae1ea7d5cde859e20dc45dbaf92fbed2
Elvis <edigitalb@gmail.com>
2016-08-26 02:01:57 -0300

    writing base

--- src/main/java/com/mageddo/bank/api/Main.java
+++ src/main/java/com/mageddo/bank/api/Main.java
@@ -2,11 +2,20 @@ package com.mageddo.bank.api;
 
 import static spark.Spark.get;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by elvis on 26/08/16.
  */
 public class Main {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
 	public static void main(String[] args) {
-		get("/hello", (request, response) -> "Hello World!");
+		get("/hello", (request, response) -> {
+			LOGGER.info("msg=hello");
+			return "Hello World!";
+		});
 	}
 }
